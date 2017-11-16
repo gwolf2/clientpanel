cb26e8ffd2f3a202eec8a17893fa11f1bbcc52ac
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-11-16 16:47:46 -0500

    Add firebase connection through angularfire2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB8ZgmTR9kNv0GurQx0KUs3xOvoT8tkuw0',
+    authDomain: 'clientpanelapp-814d2.firebaseapp.com',
+    databaseURL: 'https://clientpanelapp-814d2.firebaseio.com',
+    projectId: 'clientpanelapp-814d2',
+    storageBucket: 'clientpanelapp-814d2.appspot.com',
+    messagingSenderId: '402547044395'
+  }
 };
